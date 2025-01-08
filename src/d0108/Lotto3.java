7136b2df0d8c232834bf2811a54bc92995cba36e
Administrator <Administrator@i7E-20>
2025-01-08 12:41:53 +0900

    test

--- src/d0108/Lotto3.java
+++ src/d0108/Lotto3.java
@@ -8,13 +8,12 @@ public class Lotto3 {
 		int[] nums = new int[3];
 		Random r =new Random();
 		
-		nums[0] = r.nextInt(100);
-		System.out.println(nums[0]);
-		
+		nums[0] = r.nextInt(100); // for(int i=0;i<3;i++) for문을 사용할 수 있다.
 		nums[1] = r.nextInt(100);
-		System.out.println(nums[1]);
-
 		nums[2] = r.nextInt(100);
+		
+		System.out.println(nums[0]); // for(int i=0;i<3;i++) for문을 사용할 수 있다.
+		System.out.println(nums[1]);
 		System.out.println(nums[2]);
 	}
 
